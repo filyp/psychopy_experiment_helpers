60596a1566c6fb76d11c120292ebc714fde7b4d5
Filip Sondej <filipsondej@protonmail.com>
2022-10-03 15:16:14 +0200

    add possibility to specify trigger info from the future

--- triggers_common.py
+++ triggers_common.py
@@ -19,13 +19,17 @@ class TriggerHandler:
         self.port_eeg = port_eeg
         self.data_saver = data_saver
         self.trigger_no = 0
+        self.trial = None
 
     def prepare_trigger(self, trigger_name):
         self.trigger_no += 1
         if self.trigger_no == 9:
             self.trigger_no = 1
         line = f"{self.trigger_no}:{trigger_name}"
-        self.data_saver.triggers_list.append(line)
+        if self.trial is not None:
+            self.trial.append(line)
+        else:
+            self.data_saver.triggers_list.append(line)
 
     def send_trigger(self):
         logging.data("TRIGGER: " + self.data_saver.triggers_list[-1])
@@ -38,3 +42,12 @@ class TriggerHandler:
             except Exception as ex:
                 logging.error(ex)
                 pass
+
+    def open_trial(self):
+        self.trial = []
+
+    def close_trial(self, value):
+        for trig in self.trial:
+            trig = trig.format(value)
+            self.data_saver.triggers_list.append(trig)
+        self.trial = None
